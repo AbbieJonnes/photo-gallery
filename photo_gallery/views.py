590537af892531271d48bc37c7856a28bc8a485a
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-08 10:31:49 +0300

    Add import for messages in views.py

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 from .forms import RegistrationForm
 
